cb59a088a4357427107c69fffe520362b874218d
basnetsoyuj <bsoyuj@gmail.com>
2023-12-09 22:50:09 -0500

    Add multi-user select in split

--- web_app/app.py
+++ web_app/app.py
@@ -1,8 +1,6 @@
-from web_app.db import db
-from web_app.defaults import SECRET_KEY
+import datetime
 
 from bson.objectid import ObjectId
-
 from flask import (
     Flask,
     render_template,
@@ -10,7 +8,6 @@ from flask import (
     redirect,
     url_for
 )
-
 from flask_login import (
     LoginManager,
     UserMixin,
@@ -19,8 +16,10 @@ from flask_login import (
     current_user,
     login_required,
 )
-
 from werkzeug.security import generate_password_hash, check_password_hash
+
+from web_app.db import db, get_users
+from web_app.defaults import SECRET_KEY
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = SECRET_KEY
@@ -33,106 +32,114 @@ login_manager.init_app(app)
 # User class
 class User(UserMixin):
     def __init__(self, user_data):
-
         self.id = str(user_data['_id'])
-        self.username = user_data['username']
+        self.first_name = user_data['first_name']
+        self.last_name = user_data['last_name']
+        self.email = user_data['email']
 
 
 @login_manager.user_loader
 def load_user(user_id):
-
     user_data = db.users.find_one({'_id': ObjectId(user_id)})
     if not user_data:
         return None
     return User(user_data)
 
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-
     if request.method == 'POST':
-        if db.users.find_one({'username': request.form.get('username')}):
-            return render_template('register.html', error='Username already exists.')
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        email = request.form.get('email')
+        password = request.form.get('password')
 
-        hashed_password = generate_password_hash(
-            request.form.get("password"), method="pbkdf2:sha256"
-        )
+        if any(not field for field in [first_name, last_name, email, password]):
+            return render_template('register.html', error='Please fill all the fields.')
+        if db.users.find_one({'email': email}):
+            return render_template('register.html', error='Email already exists.')
 
-        db.users.insert_one(
-            {'username': request.form.get('username'), 'password': hashed_password}
-        )
+        hashed_password = generate_password_hash(password, method="pbkdf2:sha256")
+        db.users.insert_one({
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': email,
+            'password': hashed_password,
+            'created_at': datetime.datetime.utcnow()
+        })
 
-        user_data = db.users.find_one({'username': request.form.get('username')})
+        user_data = db.users.find_one({'email': email})
         user = User(user_data)
         login_user(user)
         return redirect(url_for('index'))
 
     return render_template('register.html')
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-
     if request.method == 'POST':
-        username = request.form.get('username')
+        email = request.form.get('email')
         password = request.form.get('password')
-        user_data = db.users.find_one({'username': username})
+        user_data = db.users.find_one({'email': email})
 
         if user_data and check_password_hash(user_data['password'], password):
             user = User(user_data)
             login_user(user)
             return redirect(url_for('index'))
-        return render_template('login.html', error='Invalid username or password.')
-    
+        return render_template('login.html', error='Invalid email or password.')
+
     return render_template('login.html')
+
 
 @app.route('/')
 def index():
-
     if current_user.is_authenticated:
-        expenses = db.expenses.find({'expense_friends': {'$in': [current_user.get_id()]}})
-        return render_template('index.html', expenses = expenses)
+        expenses = db.expenses.find({'paid_by': ObjectId(current_user.get_id())})
+        return render_template('index.html', expenses=expenses)
     return render_template('register.html')
+
 
 @app.route('/expense/<expense_id>')
 @login_required
 def expense(expense_id):
-
-    expense = db.expenses.find_one({'_id' : ObjectId(expense_id)})
+    expense = db.expenses.find_one({'_id': ObjectId(expense_id)})
 
     if (expense):
-        return render_template('expense.html', expense = expense)
+        return render_template('expense.html', expense=expense)
     else:
         return redirect(url_for('index'))
 
-@app.route('/add', methods = ['GET', 'POST'])
+
+@app.route('/add', methods=['GET', 'POST'])
 @login_required
 def add():
-
-    if (request.method == 'GET'):
-        return render_template('add.html')
-
+    if request.method == 'GET':
+        return render_template('add.html', users=get_users())
     else:
-        expense_name = request.form.get('expense_name')
-        expense_amount = int(request.form.get('expense_amount'))
-        expense_friends = request.form.get('expense_friends').strip().split(',')
+        name = request.form.get('name')
+        amount = float(request.form.get('amount'))
+        splits = request.form.get('splits').strip().split(',')
 
-        expense_friends.append(current_user.get_id())
-        per_head_cost = expense_amount/len(expense_friends)
+        splits.append(current_user.email)
+        per_head_cost = amount / len(splits)
 
         db.expenses.insert_one({
-            'expense_name': expense_name,
-            'expense_amount': expense_amount,
-            'expense_friends': expense_friends,
-            'per_head_cost': per_head_cost
+            'name': name,
+            'amount': amount,
+            'paid_by': ObjectId(current_user.get_id()),
+            'splits': {friend: per_head_cost for friend in splits}
         })
 
         return redirect(url_for('index'))
 
+
 @app.route('/logout')
 @login_required
 def logout():
-
     logout_user()
     return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(port=8001, debug=True)
